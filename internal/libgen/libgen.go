d2e70dd9b865c36b8af2e1e313ace9d057ca5f56
Temiloluwa Adebowale <65780579+stemitom@users.noreply.github.com>
2024-01-14 22:23:06 +0100

    libgen logic implementation

--- internal/libgen/libgen.go
+++ internal/libgen/libgen.go
@@ -1,1 +1,19 @@
 package libgen
+
+type Book struct {
+	Title  string
+	Author string
+	Year   int
+	URL    string
+}
+
+func SearchBook(query string) (*Book, error) {
+	// search for book from libgen
+	book := &Book{
+		Title:  "Sample Book",
+		Author: "John Doe",
+		Year:   2002,
+		URL:    "http://libgen.is/book/123456",
+	}
+	return book, nil
+}
